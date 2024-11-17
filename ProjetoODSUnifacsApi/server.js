3ca3ab5e3407e9c708a19441d8e8725a61c6ba25
Ian Menezes <129297283+ianmenezesss@users.noreply.github.com>
2024-11-17 19:06:37 -0300

    Update server.js

--- ProjetoODSUnifacsApi/server.js
+++ ProjetoODSUnifacsApi/server.js
@@ -2,12 +2,19 @@ import express from 'express'
 import publicRouter from './routes/public.js'
 import cors from 'cors'
 
+const corsOptions = {
+    origin: 'https://biosphereods.vercel.app', // Allow only your frontend's origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  };
+
 const app = express()
 app.use(express.json())
 
 // Habilita o CORS que permite que a API seja acessada por qualquer domínio
 app.use(cors())
-app.use(cors({ origin: 'https://biosphereods.vercel.app/' }));
+
+app.use(cors(corsOptions));
 
 // Rotas principais de cadastro e login
 app.use('/', publicRouter)
